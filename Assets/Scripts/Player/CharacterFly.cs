94c10f7957afe1c9dba64d6ea2181fda59b0d231
AviBoter <maccavi3@gmail.com>
2021-06-09 02:41:34 +0300

    better joystick

--- Assets/Scripts/Player/CharacterFly.cs
+++ Assets/Scripts/Player/CharacterFly.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db0e6bb830842a56497cc9929ac32841bff2c9ca30cc6adf47a3d226f7698eb7
-size 6139
+oid sha256:d5a1134e0c2c09da37c98e0b5bd2945e96db1ab9e37714227a4be85d762c2af6
+size 6307
