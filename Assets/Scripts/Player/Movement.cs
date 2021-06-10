bcc7190ee8a45dcc7098a4df4677517eaca01442
AviBoter <maccavi3@gmail.com>
2021-06-11 01:39:32 +0300

    Movement

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edb05a0e3c866a00098711919fc07b225586ea339300e19b3c6fed640defd924
-size 3855
+oid sha256:9175436a5b899fd24d583322eaf2fbbea94ad01e14e5d9be0c7268a07aa22ad1
+size 4031
