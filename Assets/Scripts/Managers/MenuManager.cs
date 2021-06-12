9c04c05d2e577e763c23300573f6786a81eb3f2a
Benitk <beny9996@gmail.com>
2021-06-12 20:20:02 +0300

    add menu, loading screen scene

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c969ecf8561103ff6257338646f558a9741ec205927c451b661b77d36d117273
+size 955
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f312bc7eee0c0fa7e9209f704565b09e040c56ec943fa66b3f99c46e62bdeae4
-size 6029
+oid sha256:a05364ba7b1db21509398d001d8403e738c146f277fa95847964cf619409c2a1
+size 6202
