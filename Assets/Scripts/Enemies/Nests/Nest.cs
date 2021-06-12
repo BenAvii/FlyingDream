a8908921a429de8d263c46b3e4103e86724a3b07
AviBoter <maccavi3@gmail.com>
2021-06-13 01:06:58 +0300

    New_Movement

--- Assets/Scripts/Enemies/Nests/Nest.cs
+++ Assets/Scripts/Enemies/Nests/Nest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79cb5b7175433e83010c3087e0b82469b1582534fc0b3144df956547eaaefa2a
-size 1742
+oid sha256:60c9903212e08e939d14f610698e65b93a67f2ddfe33ba818205fa44eb9aac3a
+size 2045
---
+++ Assets/Scripts/Player/Movement2.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aef4d0c1e221c7309fc1234c341eee219b9807d25fdab1333358593fea6a2e25
+size 2812
