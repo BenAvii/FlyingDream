2f82bb71331b8809e085f94f1b0444f64be6e746
Benitk <beny9996@gmail.com>
2021-06-12 18:42:51 +0300

    add UI

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a160a466f18e3120ed0880e51541648c6d9c538b24e58fe8060c48ea8162252
+size 1433
---
+++ Assets/Scripts/Managers/Ignore.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7255431ad0fc4d904d2c2d9ae277dccc7ed4cb5b55bbbc5ad18069976b1e329e
+size 308
